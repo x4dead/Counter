a8888c4b096c3c9b1bab6e9c9a35b0a6557406fe
x4dead <muratovargen0@gmail.com>
2022-10-21 23:55:01 +0600

    init

---
+++ lib/bloc/counter_bloc.dart
@@ -0,0 +1,52 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterBloc extends Bloc<CounterEvents, CounterStates> {
+  CounterBloc() : super(CounterInit()) {
+    on<AddTwo>((event, emit) {
+      ++index == index++;
+      emit(CounterSucces());
+    });
+    on<ReduceOne>((event, emit) {
+      if (index > 0) {
+        --index;
+      } else {
+        null;
+      }
+      emit(CounterSucces());
+    });
+    on<ClearAll>((event, emit) {
+      index = 0;
+      emit(CounterSucces());
+    });
+
+    // on<ChangeTheme>((event, emit) {      //Не удавшаяся функция(
+    //   if (theme != ThemeData.dark()) {
+    //     theme = ThemeData.dark();
+    //     emit(CounterSucces());
+    //   } else {
+    //     theme = ThemeData.light();
+    //   }
+    //   print(theme);
+    // });
+  }
+  int index = 0;
+  // ThemeData theme = ThemeData.light();
+}
+
+abstract class CounterEvents {}
+
+class AddTwo extends CounterEvents {}
+
+class ReduceOne extends CounterEvents {}
+
+class ClearAll extends CounterEvents {}
+
+class ChangeTheme extends CounterEvents {}
+
+//
+abstract class CounterStates {}
+
+class CounterInit extends CounterStates {}
+
+class CounterSucces extends CounterStates {}
---
+++ lib/home_page.dart
@@ -0,0 +1,98 @@
+﻿import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test/bloc/counter_bloc.dart';
+
+// part 'widgets/app_bar.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final blocProvider = BlocProvider.of<CounterBloc>(context);
+    return BlocBuilder<CounterBloc, CounterStates>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Тестовое задание: Счетчик'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '${blocProvider.index}',
+                style: const TextStyle(fontSize: 24),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  FloatingActionButton(
+                    tooltip: 'Добавить 2',
+                    backgroundColor: Colors.blueAccent,
+                    onPressed: () {
+                      blocProvider.add(AddTwo());
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '+',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          '2',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  FloatingActionButton(
+                    tooltip: 'Отнять 1',
+                    backgroundColor: Colors.red,
+                    onPressed: () {
+                      blocProvider.add(ReduceOne());
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          '-',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          '1',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: Colors.lightGreen,
+            onPressed: () {
+              blocProvider.add(ClearAll());
+            },
+            label: Row(
+              children: const [
+                Text(
+                  'Очистить счетчик',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 5),
+                Icon(
+                  Icons.delete,
+                  size: 20,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
